0fa38384552a703b025dc2452621d306eb55ef6a
wonpyo_kwon <wonpyo_kwon@tmax.co.kr>
2023-06-01 12:24:33 +0900

    테스트 코드 수정

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Michael D'Angelo");
+    expect(document.title).toBe('권원표 홈페이지');
   });
 
   it('can navigate to /about', async () => {
